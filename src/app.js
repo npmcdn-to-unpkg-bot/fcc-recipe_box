0cfa35daf4dd8c2a7115c6111bf907b3c38246ff
Viktors Jenovs <jviktors@gmail.com>
2016-08-13 20:26:03 +0300

    Add basic layout and modal

--- src/app.js
+++ src/app.js
@@ -1,15 +1,20 @@
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+
 // Setting localStorage for testing purposes
 // localStorage.setItem("recipes", '[{"title": "testMeal_1", "ingredients": ["ingredient_1", "ingredient_2"]}, {"title": "testMeal_2", "ingredients": ["ingredient_1", "ingredient_2"]}]');
 
 class Layout extends React.Component {
   constructor() {
     super();
-    const url = "sample_recipes.json";
+    this.state = {"recipes": []};
+  }
+
+  componentDidMount() {
     const recipes = JSON.parse(localStorage.getItem("recipes"));
-    this.state = recipes ? {"recipes": recipes} : {"recipes": []};
-    if (!this.state.recipes.length) {
-      this.getJson(url);
-    }
+    const url = "./sample_recipes.json";
+
+    (!recipes) ? this.getJson(url) : this.setState({recipes});
   }
 
   getJson(url) {
@@ -27,12 +32,175 @@ class Layout extends React.Component {
   render() {
     return (
       <div>
-      {JSON.stringify(this.state.recipes)}
+        <Recipes data={this.state.recipes} />
+        <AddRecipeButton />
       </div>
+    );
+  }
+}
+
+class Recipes extends React.Component {
+
+  getRecipes() {
+    let result = [];
+    for (let i = 0; i < this.props.data.length; i++) {
+      result.push(<Recipe key={i} data={this.props.data[i]}/>)
+    }
+    return result;
+  }
+
+  render() {
+    const recipes = this.getRecipes();
+    return (
+      <div className="recipes">
+        {recipes}
+      </div>
+    );
+  }
+}
+
+class Recipe extends React.Component {
+
+  getIngredients() {
+    let result = [];
+    for (let i = 0; i < this.props.data.length; i++) {
+      result.push(<Ingredients key={i} data={this.props.data[i]["ingredients"]} />);
+
+      // this.setState({title: this.props.data[i]["title"]});
+    }
+    console.log(result);
+    return result;
+  }
+  render() {
+    console.log("Recipe, render, this.props", this.props);
+    // const title = this.props.data.title;
+    const ingredients = this.getIngredients();
+    return (
+      <div>
+        <RecipeTitle title={this.props.data.title} />
+        <Ingredients data={this.props.data["ingredients"]} />
+      </div>
+    );
+  }
+}
+
+class RecipeTitle extends React.Component {
+  render() {
+    console.log("RecipeTitle props", this.props.data);
+    // console.log("RecipeTitle", this.props.data[0].title);
+    return (
+      <div>
+        {this.props.title}
+      </div>
+    );
+  }
+}
+
+class Ingredients extends React.Component {
+  parseData() {
+    const dataArr = this.props.data;
+    let result = [];
+    for(let i = 0; i < dataArr.length; i++) {
+      result.push(<div key={i}>{dataArr[i]}</div>)
+    }
+    return result;
+  }
+  render() {
+    console.log("Ingredients", this.props.data);
+    const ingredients = this.parseData();
+    return (
+      <div>
+        {ingredients}
+      </div>
+    );
+  }
+}
+
+class AddRecipeButton extends React.Component {
+  constructor() {
+    super();
+    this.state = {display: "none"}
+    // this.state = {display: "block"}
+  }
+
+  openModal() {
+    console.log("clicked");
+    this.setState({display: "block"});
+  }
+  closeModal() {
+    this.setState({display: "none"});
+  }
+  render() {
+    console.log("AddRecipeButton this.state.display", this.state.display);
+    const display = this.state.display;
+    return (
+      <div className="mod">
+      <button onClick={() => this.openModal()}>
+        Add Recipe
+      </button>
+      <Modal display={this.state.display} closeModal={this.closeModal.bind(this)}/>
+    </div>
+    );
+  }
+}
+
+class Modal extends React.Component {
+
+  constructor() {
+    super();
+    this.state = {display: "none"};
+    window.addEventListener("click", (e) => {
+      console.log(e);
+      if(e.target.parentElement.className !== "mod") {
+        this.props.closeModal()
+      }
+      event.stopPropagation();
+    });
+  }
+
+  render() {
+    const display = this.props.display;
+    return (
+      <div style={{display: display}} className="mod modal">
+        <ModalTitle />
+        <ModalInput />
+        <ModalInput />
+        <CloseButton closeModal={this.props.closeModal}/>
+      </div>
+    );
+  }
+}
+
+class ModalTitle extends React.Component {
+  render() {
+    return (
+      <div>
+        ModalTitle
+      </div>
+    );
+  }
+}
+
+class ModalInput extends React.Component {
+  render() {
+    return (
+      <div>
+        ModalInput
+      </div>
+    );
+  }
+}
+
+class CloseButton extends React.Component {
+  render() {
+    return (
+      <button onClick={this.props.closeModal}>
+        Close
+      </button>
     );
   }
 }
 
 ReactDOM.render(
   <Layout />, document.getElementById('app')
-);
+  );
