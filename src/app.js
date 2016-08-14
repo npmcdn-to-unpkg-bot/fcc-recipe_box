9dae38f46f401a385bc5d6ccbfccc73c8deada23
Viktors Jenovs <jviktors@gmail.com>
2016-08-14 06:25:00 +0300

    Move Modal back to app.js

--- src/app.js
+++ src/app.js
@@ -145,6 +145,79 @@ class AddRecipeButton extends React.Component {
   }
 }
 
+class Button extends React.Component {
+    render() {
+      return (
+        <button onClick={this.props.clicked}>
+          {this.props.title}
+        </button>
+      );
+    }
+}
+Button.defaultProps = {type: "button", title: "Button", clicked: () => {console.log("click");}};
+
+class Modal extends React.Component {
+
+  constructor() {
+    super();
+    this.state = {display: "none"};
+    window.addEventListener("click", (e) => {
+      if(e.target.id === "modal") {
+        this.props.closeModal();
+      }
+    });
+    //close modal on "Esc"
+    document.addEventListener('keyup', (e) => {
+      if (e.keyCode === 27) {
+        this.props.closeModal();
+      }
+    });
+  }
+
+  render() {
+    const display = this.props.display;
+    return (
+      <div id="modal" style={{display: display}} className="modal-dialog">
+        <div className="modal-content">
+        <ModalTitle title={this.props.title}/>
+        <ModalInput />
+        <ModalInput />
+        <Button title={"Add"} />
+        <Button title={"Close"} clicked={this.props.closeModal}/>
+        </div>
+      </div>
+    );
+  }
+}
+
+class ModalTitle extends React.Component {
+
+  render() {
+    return (
+      <div>
+        {this.props.title}
+      </div>
+    );
+  }
+}
+
+ModalTitle.defaultProps = {title: "ModalTitle"};
+
+class ModalInput extends React.Component {
+
+  render() {
+    return (
+      <div>
+        {this.props.title}
+        <input placeholder={this.props.placeholder}>
+        </input>
+      </div>
+    );
+  }
+}
+
+ModalInput.defaultProps = {title: "Title", placeholder: "Input here..."};
+
 ReactDOM.render(
   <Layout />, document.getElementById('app')
   );
